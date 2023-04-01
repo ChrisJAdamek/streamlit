e2a6796862c3c5f4495d96bb08d8a156e8b20a7f
Chris <104967421+ChrisJAdamek@users.noreply.github.com>
2023-04-01 17:18:30 +1100

    Update streamlit_openai_app.py

--- streamlit_openai_app.py
+++ streamlit_openai_app.py
@@ -4,7 +4,7 @@ import streamlit as st
 import openai
 
 # Set up OpenAI API
-openai.api_key = os.environ["OPENAI_API_KEY"]
+openai.api_key = st.secrets["OPENAI_API_KEY"]
 
 # Streamlit app
 st.title("Streamlit OpenAI App")
