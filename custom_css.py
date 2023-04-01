2abf338763ed5a5a947f2e2aa4f7da5d69045c63
Chris <104967421+ChrisJAdamek@users.noreply.github.com>
2023-04-02 01:19:32 +1100

    Create custom_css.py

---
+++ custom_css.py
@@ -0,0 +1,36 @@
+custom_css = """
+<style>
+    body {
+        background-color: #f7f7f7;
+    }
+    .stApp {
+        padding: 2rem;
+    }
+    h1 {
+        font-family: 'Pirata One', cursive;
+        font-size: 3rem;
+    }
+    .accessible-text {
+        color: #4a4a4a;
+    }
+    .user-bubble {
+        background-color: #34C759;
+        padding: 1rem;
+        margin-bottom: 0.5rem;
+        border-radius: 1rem;
+        max-width: 75%;
+        color: white;
+    }
+    .pirate-bubble {
+        background-color: #0A84FF;
+        padding: 1rem;
+        margin-bottom: 0.5rem;
+        border-radius: 1rem;
+        max-width: 75%;
+        color: white;
+    }
+    textarea {
+        resize: vertical;
+    }
+</style>
+"""
