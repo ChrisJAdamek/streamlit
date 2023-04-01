6ad74fa52f5439a0a9f3bc259b91d191df997b7e
Chris <104967421+ChrisJAdamek@users.noreply.github.com>
2023-04-02 02:05:39 +1100

    Update custom_css.py

--- custom_css.py
+++ custom_css.py
@@ -1,42 +1,43 @@
-import streamlit as st
-
-custom_css = """
 <style>
-    body {
-        background-color: #f7f7f7;
+  body {
+    background-color: #000000;
+    font-family: 'Courier New', monospace;
+  }
+
+  .stApp {
+    padding: 2rem;
+  }
+
+  .chat-container {
+    background-color: #000000;
+    border: 2px solid #00ff00;
+    border-radius: 10px;
+    padding: 20px;
+    max-width: 800px;
+    margin: 0 auto;
+    overflow-y: scroll;
+    height: 500px;
+  }
+
+  .message {
+    color: #00ff00;
+    white-space: nowrap;
+    overflow: hidden;
+    animation: typing 2s steps(30, end), blink-caret 0.5s step-end infinite;
+  }
+
+  @keyframes typing {
+    from {
+      width: 0;
     }
-    .stApp {
-        padding: 2rem;
+    to {
+      width: 100%;
     }
-    h1 {
-        font-family: 'Pirata One', cursive;
-        font-size: 3rem;
+  }
+
+  @keyframes blink-caret {
+    50% {
+      border-color: transparent;
     }
-    .accessible-text {
-        color: #4a4a4a;
-    }
-    .user-bubble {
-        background-color: #34C759;
-        padding: 1rem;
-        margin-bottom: 0.5rem;
-        border-radius: 1rem;
-        max-width: 75%;
-        color: white;
-    }
-    .pirate-bubble {
-        background-color: #0A84FF;
-        padding: 1rem;
-        margin-bottom: 0.5rem;
-        border-radius: 1rem;
-        max-width: 75%;
-        color: white;
-    }
-    textarea {
-        resize: vertical;
-    }
+  }
 </style>
-"""
-
-st.markdown(custom_css, unsafe_allow_html=True)
-
-st.markdown("<link href='https://fonts.googleapis.com/css?family=Pirata+One' rel='stylesheet'>", unsafe_allow_html=True)
