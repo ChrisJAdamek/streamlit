d08f0f006e4d5a48cb6730a0ce5154ef2d49ed91
Chris <104967421+ChrisJAdamek@users.noreply.github.com>
2023-04-01 16:01:12 +1100

    Create streamlit_openai_app.py

---
+++ streamlit_openai_app.py
@@ -0,0 +1,32 @@
+import os
+import requests
+import streamlit as st
+import openai
+
+# Set up OpenAI API
+openai.api_key = os.environ["OPENAI_API_KEY"]
+
+# Streamlit app
+st.title("Streamlit OpenAI App")
+st.write("This app connects to the OpenAI API to generate text based on user input.")
+
+user_input = st.text_input("Enter some text to get a response from OpenAI:")
+
+max_tokens_options = [50, 100, 150, 200]
+max_tokens = st.radio("Select max tokens:", max_tokens_options)
+
+temperature = st.slider("Select temperature:", min_value=0.0, max_value=1.0, value=0.8, step=0.1)
+
+if user_input:
+    try:
+        response = openai.Completion.create(
+            engine="text-davinci-002",
+            prompt=user_input,
+            max_tokens=max_tokens,
+            n=1,
+            stop=None,
+            temperature=temperature,
+        )
+        st.write(response.choices[0].text.strip())
+    except Exception as e:
+        st.write("Error:", str(e))
