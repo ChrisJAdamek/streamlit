50de184f756649ff845018a331336ded0d19a3ef
Chris <104967421+ChrisJAdamek@users.noreply.github.com>
2023-04-01 18:06:47 +1100

    Update streamlit_openai_app.py

--- streamlit_openai_app.py
+++ streamlit_openai_app.py
@@ -9,7 +9,7 @@ openai.api_key = st.secrets["OPENAI_API_KEY"]
 # Define the function to send a message to OpenAI
 def send_message_to_openai(prompt, max_tokens, temperature):
     response = openai.Completion.create(
-        engine="text-davinci-002",
+        engine="text-davinci-003",
         prompt=prompt,
         max_tokens=max_tokens,
         n=1,
@@ -29,7 +29,7 @@ pre_prompt = "please pretend you are a pirate in all future responses."
 user_message = st.text_input("Enter your message:")
 
 # Add sliders for max_tokens and temperature
-max_tokens = st.slider("Max tokens:", min_value=10, max_value=200, value=100, step=10)
+max_tokens = st.slider("Max tokens:", min_value=10, max_value=1000, value=100, step=10)
 temperature = st.slider("Temperature:", min_value=0.1, max_value=1.0, value=0.7, step=0.1)
 
 if user_message:
