94c96ef54ae99acd0905e87e7b1a236c039703f3
Chris <104967421+ChrisJAdamek@users.noreply.github.com>
2023-04-01 19:21:12 +1100

    Update streamlit_openai_app.py

--- streamlit_openai_app.py
+++ streamlit_openai_app.py
@@ -19,6 +19,27 @@ def send_message_to_openai(prompt, max_tokens, temperature):
 
     return response.choices[0].text.strip()
 
+# Custom CSS for the app
+custom_css = """
+<style>
+    body {
+        background-color: #f7f7f7;
+    }
+    .stApp {
+        padding: 2rem;
+    }
+    h1 {
+        font-family: 'Pirata One', cursive;
+        font-size: 3rem;
+    }
+</style>
+"""
+
+st.markdown(custom_css, unsafe_allow_html=True)
+
+# Load custom font
+st.markdown("<link href='https://fonts.googleapis.com/css?family=Pirata+One' rel='stylesheet'>", unsafe_allow_html=True)
+
 # Streamlit app starts here
 st.title("Pirate Chatbot")
 
@@ -29,8 +50,9 @@ pre_prompt = "please pretend you are a pirate in all future responses."
 user_message = st.text_input("Enter your message:", height=100)
 
 # Add sliders for max_tokens and temperature
-max_tokens = st.slider("Max tokens:", min_value=10, max_value=1000, value=100, step=10)
-temperature = st.slider("Temperature:", min_value=0.1, max_value=1.0, value=0.7, step=0.1)
+with st.expander("Advanced Settings", expanded=False):
+    max_tokens = st.slider("Max tokens:", min_value=10, max_value=1000, value=100, step=10)
+    temperature = st.slider("Temperature:", min_value=0.1, max_value=1.0, value=0.7, step=0.1)
 
 if user_message:
     # Combine the pre-prompt and the user message
@@ -39,5 +61,5 @@ if user_message:
     # Send the combined prompt to OpenAI with the specified max_tokens and temperature
     response = send_message_to_openai(combined_prompt, max_tokens, temperature)
 
-    # Display the response
-    st.write(response)
+    # Display the response in a container with a pirate-themed background
+    st.markdown(f"<div style='background-image: url(https://i.imgur.com/3CgjYd4.jpg); padding: 2rem; color: white; font-size: 1.2rem; border-radius: 10px;'>{response}</div>", unsafe_allow_html=True)
